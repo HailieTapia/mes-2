87e11db3d814b7680c5af25bb09daa4699cdc26b
Hailie <htapiahailie@gmail.com>
2025-06-25 17:29:56 -0600

    Initial commit

--- app.py
+++ app.py
@@ -1,27 +1,23 @@
 from flask import Flask, render_template, request
-import pickle
+import joblib
 import numpy as np
 
 app = Flask(__name__)
 
-# Cargar el modelo entrenado
-with open('modelo_Random.pkl', 'rb') as file:
-    model = pickle.load(file)
+# Cargar el modelo entrenado con joblib
+model = joblib.load('modelo_Random.pkl')
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
     prediction = None
     if request.method == 'POST':
-        # Obtener los datos del formulario
         displacement = float(request.form['displacement'])
         weight = float(request.form['weight'])
         model_year = float(request.form['model_year'])
         horsepower = float(request.form['horsepower'])
         
-        # Preparar los datos en el formato esperado por el modelo
         input_data = np.array([[displacement, weight, model_year, horsepower]])
         
-        # Hacer la predicción
         prediction = model.predict(input_data)[0]
     
     return render_template('index.html', prediction=prediction)
